ab9ed938f38f16c7868cf422dee2e802812e2f68
rml3 <derekfan.18.2002@gmail.com>
2026-03-17 14:53:41 -0400

    reduce regularization

--- tdmpc2/tdmpc2.py
+++ tdmpc2/tdmpc2.py
@@ -292,9 +292,13 @@ class TDMPC2(torch.nn.Module):
 			# Evidential egularization
 			aleatoric = aleatoric_uncertainty(nu, alpha, beta)
 			reg_loss = torch.abs((_next_z - gamma) / aleatoric) * (2 * nu + alpha)
-			LAM_MAX = 1e-2
-			LAM_MAX_RATIO = 0.5
-			lam = LAM_MAX * min(1, self.step / (self.cfg.steps * LAM_MAX_RATIO))
+			LAM_MAX = 1e-4
+			LAM_START = 0.1 * self.cfg.steps
+			LAM_RAMP_RATIO = 0.5
+			if self.step <= LAM_START:
+				lam = 0
+			else:
+				lam = LAM_MAX * min(1, (self.step - LAM_START)  / (self.cfg.steps * LAM_RAMP_RATIO))
 
 			# Aggregate batched losses
 			loss_change = (nll_loss + lam * reg_loss).mean()
